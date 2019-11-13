e348a75e1c37081d27172fff171f02d6810cb325
saint-yellow <saint-yellow@hotmail.com>
2019-11-13 23:32:45 +0800

    添加注释

--- crawl_html.py
+++ crawl_html.py
@@ -36,16 +36,20 @@ def get_html(url: str) -> str:
 
 
 def collect_urls(current_url: str):
+    '''收集链接, 将链接写入到txt文件中'''
+
+    # 现将当前的URL写入到txt文件中
     with open('urls.txt', 'a', encoding='utf-8') as f:
         f.write(current_url + '\n')
 
+    # 解析当前页面的HTML内容, 寻找指向下一页面的a标签
     html = get_html(current_url)
     soup = BeautifulSoup(html, 'html.parser')
     next_link = soup.find('a', {'id': 'next-link'})
 
     if next_link is not None:
+        # 找到指向下一页的a标签, 根据该标签的href值构造下一页面的URL
         next_href : str = next_link['href']
-
         next_url = ''
         if next_href.startswith('../'):
             next_url = HOMEPAGE + next_href.replace('../', '')
@@ -54,13 +58,17 @@ def collect_urls(current_url: str):
             url_parts[-1] = next_href
             next_url = '/'.join(url_parts)
 
+        # 递归调用自身, 像处理当前的URL那样处理下一个URL
         collect_urls(next_url)
     else:
+        # 找不到指向下一页的a标签, 表名当前页面是最后一页, 爬取完成
         print('Completed crawling.')
         return
 
 
 def generate_pdf():
+    '''根据爬取得来的链接, 生成PDF文件'''
+
     with open('urls.txt', 'r', encoding='utf-8') as f:
         urls = f.readlines()
         for index, url in enumerate(urls):
@@ -79,4 +87,5 @@ def main():
     generate_pdf()
 
 
-main()
+if __name__ == '__main__':
+    main()
